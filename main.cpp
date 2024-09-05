2aee8b4d688f44438f9d25623905c763c3e38927
Karol Typiak <ktypiak@wp.pl>
2024-09-05 19:08:52 +0200

    cos

---
+++ main.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+
+int main(){
+  std::cout << "¥¥¥¥¥";
+  
+  return 0;
+}
+
+// int - integer, caˆe liczby
+// (long) double, liczby z przecinkiem (kropk¥)
+// char - pojedyncze znaki ('A')
+// boolean - true/false, tak/nie, 1/0
+// std::string - ci¥g znak¢w ("Aha xd")
+// 
+// const double LIGHT_SPEED=299792458 - tylko do odczytu, nie da si© zmieni† (nazwy z du¾ych liter)
+// 
+// 
+// std::cout << "aha";      nowa linia- << endl << "\n";
+// std::cin >> x;
+// namespace - provides a solution for preventing name conflicts in large projects. Each entity needs a  unique name. A namespace allows for iden4cally named entities as long as the namespaces are different.
+// 
+// namespace jeden{     namespace dwa{      
+// int x = 1;           int x = 2;                          std::cout << jeden::x;      using std::cout;
+// }                    }                   albo na g¢rze   using namespace jeden;      using std::string;
+// 
+// typedef/using - reserved keyword used to create an additional name (alias) for another data type. New identifier for an existing type Helps with readability and reduces typos Use when there is a clear benefit Replaced with ' using' (work better w/ templates)
+// 
+// typedef std::string text_t;
+// typedef int numer_t;
+// 
+// using text_t = std::string;
+// using numer_t = int;
+// 
+// main(){
+// text_t imi© = "Karol";
+// numer_t wiek = 12;
+// }
+//   x = x + 1      x+=1      x++ 
+//   x = x + 2      x+=2
+//  
+//   double x = (int) 3.14  
+// 
+//  std::getline(std::cin>>std::ws, name);         >>ws ekstraktuje white spaces np. \n 
+// 
+// ********maths: https://cplusplus.com/reference/cmath/ ***********
+//  
+// if () {}
+// else if (){}
+// else {}
+// variable = (condition) ? expressionTrue : expressionFalse;          kr¢tka wersja
+// 
+// switch(){
+//  case 1:           liczby
+//
+//  brake;
+// }
+//  
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
